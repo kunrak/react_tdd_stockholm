fa3d2748d66603f3f14b5dc11ddddc8296fb3262
kunrak <rakeshkunducsgi@gmail.com>
2024-01-06 13:35:16 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Recipes from './components/Recipes';
+
+function App() {
+  return (
+    <div className="App">
+      <Recipes />
+    </div>
+  );
+}
+
+export default App;
